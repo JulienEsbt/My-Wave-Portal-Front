8c57947c633f8324bf20fb7a7b239fbab37124a1
Julien Esterbet <julien.esterbet@sib.fr>
2022-04-27 07:38:23 +0000

    Update src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,55 +12,55 @@ const App = () => {
    */
   const contractABI = abi.abi;
 
-	const checkIfWalletIsConnected = async () => {
-		try {
-			const { ethereum } = window;
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
 
-			if (!ethereum) {
-				console.log('Make sure you have metamask!');
-				return;
-			} else {
-				console.log('We have the ethereum object', ethereum);
-			}
+      if (!ethereum) {
+        console.log("Make sure you have metamask!");
+        return;
+      } else {
+        console.log("We have the ethereum object", ethereum);
+      }
 
-			const accounts = await ethereum.request({ method: 'eth_accounts' });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-			if (accounts.length !== 0) {
-				const account = accounts[0];
-				console.log('Found an authorized account:', account);
-				setCurrentAccount(account);
-			} else {
-				console.log('No authorized account found');
-			}
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      if (accounts.length !== 0) {
+        const account = accounts[0];
+        console.log("Found an authorized account:", account);
+        setCurrentAccount(account);
+      } else {
+        console.log("No authorized account found")
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	/**
-	 * Implement your connectWallet method here
-	 */
-	const connectWallet = async () => {
-		try {
-			const { ethereum } = window;
+  /**
+  * Implement your connectWallet method here
+  */
+  const connectWallet = async () => {
+    try {
+      const { ethereum } = window;
 
-			if (!ethereum) {
-				alert('Get MetaMask!');
-				return;
-			}
+      if (!ethereum) {
+        alert("Get MetaMask!");
+        return;
+      }
 
-			const accounts = await ethereum.request({
-				method: 'eth_requestAccounts'
-			});
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts" 
+      });
 
-			console.log('Connected', accounts[0]);
-			setCurrentAccount(accounts[0]);
-		} catch (error) {
-			console.log(error);
-		}
-	};
+      console.log("Connected", accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-	const wave = async () => {
+  const wave = async () => {
     try {
       const { ethereum } = window;
 
@@ -91,34 +91,43 @@ const App = () => {
     }
   }
 
-	useEffect(() => {
-		checkIfWalletIsConnected();
-	}, []);
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
 
-	return (
-		<div className="mainContainer">
-			<div className="dataContainer">
-				<div className="header">👋 Hey there!</div>
+  return (
+    <div className="mainContainer">
+      <div className="dataContainer">
+        <div className="header">
+        👋 Yo les gars !
+        </div>
 
-				<div className="bio">
-					Création du premier smart contract : envoie un message !
-				</div>
+        <div className="bio">
+          Ceci est mon premier smart contract !! Vas y clique, balance une transaction dans la blockchain !
+        </div>
 
-				<button className="waveButton" onClick={wave}>
-					Wave at Me
-				</button>
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
 
-				{/*
+        {/*
         * If there is no currentAccount render this button
         */}
-				{!currentAccount && (
-					<button className="waveButton" onClick={connectWallet}>
-						Connect Wallet
-					</button>
-				)}
-			</div>
-		</div>
-	);
-};
+        {!currentAccount && (
+          <button className="waveButton" onClick={connectWallet}>
+            Connect Wallet
+          </button>
+        )}
 
-export default App;
+        <a href="https://rinkeby.etherscan.io/address/0x0da1f4a98ed55aae34c0ae7078a08a3bfe7aaee5" target="_blank" onclick="fonction(this.href); return false;">
+	        <button className="waveButton">
+		        Voir sur Etherscan
+	        </button>
+        </a>
+            
+      </div>
+    </div>
+  );
+}
+
+export default App
